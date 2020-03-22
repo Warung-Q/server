a24d5ff36b20a362240efc30904522b37f16d1ec
yufias <yupi41@gmail.com>
2020-03-22 14:46:30 +0700

    google-passport returning email and token

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ const cors = require('cors')
 const routes = require('./router')
 const errorHandler = require('./middlewares/errorHandler')
 app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
 app.use(express.json())
 app.use(cors())
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+
+app.get('/', (req, res) => {
+  res.send('home')
+})
 
 app.use('/', routes)
-
 app.use(errorHandler)
+
 
 module.exports = app
--- router/owner.js
+++ router/owner.js
@@ -1,8 +1,34 @@
 const router = require('express').Router()
 const OwnerController = require('../controllers/OwnerController')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+
+
+router.use(passport.initialize());
+router.use(passport.session());
 
 router.post('/register', OwnerController.register)
 
 router.post('/login', OwnerController.login)
+
+router.get('/authlogin', (req, res) => {
+  res.render('login')
+})
+
+router.get('/login/google', OwnerController.googlePassport)
+
+router.get('/return/google', passport.authenticate('google', { succesRedirect: '/', failureRedirect: '/authlogin' }))
+
+
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
 
 module.exports = router
