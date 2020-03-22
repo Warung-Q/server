68d098b8c954ebea705e5f4d6b87cf7a6a01aeae
baufakhran <baufakhran@gmail.com>
2020-03-22 17:25:56 +0700

    after test oauth

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const cors = require('cors')
 const routes = require('./router')
 const errorHandler = require('./middlewares/errorHandler')
 app.use(express.urlencoded({ extended: false }))
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(cors())
-app.use(require('morgan')('combined'));
-app.use(require('cookie-parser')());
-app.use(require('body-parser').urlencoded({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
-
+app.use(require('morgan')('combined'))
+app.use(require('cookie-parser')())
+app.use(require('body-parser').urlencoded({ extended: true }))
+app.use(
+  require('express-session')({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true
+  })
+)
 
 app.get('/', (req, res) => {
   res.send('home')
@@ -22,6 +27,5 @@ app.get('/', (req, res) => {
 
 app.use('/', routes)
 app.use(errorHandler)
-
 
 module.exports = app
