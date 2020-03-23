54c32d5ed3043bfc01c446ac684877414f177a10
baufakhran <baufakhran@gmail.com>
2020-03-23 16:59:58 +0700

    add readmi

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const routes = require('./router')
 const errorHandler = require('./middlewares/errorHandler')
+// const cron = require('./cron/cron')
 app.use(express.urlencoded({ extended: false }))
 app.set('view engine', 'ejs')
 app.use(express.json())
@@ -20,6 +21,12 @@ app.use(
     saveUninitialized: true
   })
 )
+
+app.get('/', (req, res) => {
+  res.send('home')
+})
+
+// app.get(cron)
 
 app.use('/', routes)
 app.use(errorHandler)
