664924a03111d55c0c595e08272da421cb9d6777
yufias <yupi41@gmail.com>
2020-03-24 20:02:15 +0700

    minor add for OAuth

--- controllers/OwnerController.js
+++ controllers/OwnerController.js
@@ -2,9 +2,6 @@ const { sequelize, Owner, Warung } = require('../models')
 const jwt = require('jsonwebtoken')
 const private_key = process.env.PRIVATEKEY
 const bcrypt = require('../helpers/bcrypt')
-// const passport = require('passport')
-// const FacebookStrategy = require('passport-facebook').Strategy
-// const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
 const {
   OAuth2Client
 } = require('google-auth-library');
@@ -90,15 +87,14 @@ class OwnerController {
   }
 
   static googleSignin(req, res, next) {
-    console.log("masuk ke google controller")
     let payload
+    let token
     client.verifyIdToken({
             idToken: req.headers.access_token,
             audience: process.env.GCLIENTID
         })
         .then(result => {
             payload = result.getPayload();
-            console.log(result, "==========")
             return Owner.findOne({
                 where: {
                     email: payload.email
@@ -120,77 +116,24 @@ class OwnerController {
                 email: data.email,
                 WarungId: data.Warung.id
             };
-            let token = jwt.sign({
+            token = jwt.sign({
                 payload
             }, private_key);
-            res.status(200).json({
-                token
-            });
+            return Warung.create({
+              name: data.username,
+              OwnerId: data.id
+            })
+        })
+        .then(result => {
+          res.status(200).json({
+            token
+          });
         })
         .catch(err => {
             console.log(err, "dari error google sign in")
             next(err);
         });
   }
-
-  // static googlePassport(req, res, next) {
-  //   console.log('Masuk ke controller')
-  //   passport.use(
-  //     new GoogleStrategy(
-  //       {
-  //         clientID: process.env.GCLIENTID,
-  //         clientSecret: process.env.GCLIENTSECRET,
-  //         callbackURL: '/owner/return/google'
-  //       },
-  //       function(accessToken, refreshToken, profile, callback) {
-  //         Owner.findOne({
-  //           where: {
-  //             email: profile.emails[0].value
-  //           }
-  //         })
-  //           .then(data => {
-  //             if (!data) {
-  //               return Owner.create({
-  //                 username: profile.displayName,
-  //                 email: profile.emails[0].value,
-  //                 password: process.env.GCLIENTSECRET
-  //               })
-  //             } else {
-  //               console.log(data, ']]]]]]]]]]Data udah ada')
-  //               return data
-  //             }
-  //           })
-  //           .then(result => {
-  //             let payload = {
-  //               id: result.id,
-  //               email: result.email
-  //             }
-  //             let token = jwt.sign(
-  //               {
-  //                 payload
-  //               },
-  //               private_key
-  //             )
-  //             res.status(200).json({
-  //               token
-  //             })
-  //             console.log(token, '[[[[[[[[[[[[[')
-  //           })
-  //           .catch(err => {
-  //             console.log(err, 'dari error google sign in')
-  //           })
-  //         console.log(profile, '>>>>>>>>profilegoogle')
-  //       }
-  //     )
-  //   )
-
-  //   passport.authenticate('google', {
-  //     scope: [
-  //       'https://www.googleapis.com/auth/plus.login', //dari dokumentasi
-  //       'https://www.googleapis.com/auth/userinfo.email' //buat ngambil email
-  //     ]
-  //   })(req, res, next)
-  // }
 }
 
 module.exports = OwnerController
--- router/owner.js
+++ router/owner.js
@@ -1,36 +1,10 @@
 const router = require('express').Router()
 const OwnerController = require('../controllers/OwnerController')
-const passport = require('passport')
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-
-// router.use(passport.initialize())
-// router.use(passport.session())
 
 router.post('/register', OwnerController.register)
 
 router.post('/login', OwnerController.login)
 
 router.post('/google_signin', OwnerController.googleSignin)
-
-// router.get('/authlogin', (req, res) => {
-//   res.render('login')
-// })
-
-// router.get('/login/google', OwnerController.googlePassport)
-
-// router.get(
-//   '/return/google',
-//   passport.authenticate('google', {
-//     failureRedirect: '/authlogin'
-//   })
-// )
-
-// passport.serializeUser(function(user, cb) {
-//   cb(null, user)
-// })
-
-// passport.deserializeUser(function(obj, cb) {
-//   cb(null, obj)
-// })
 
 module.exports = router
